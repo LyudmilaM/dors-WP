37c074e6ae043b44b61a09cca2617098c6e6f812
LyudmilaM <Minhludmila@gmail.com>
2023-02-15 12:28:12 +1000

    Создали папку, где будут храниться шаблоны страниц, перенесли туда файлы html. В header.php сделали возможность менять title из админки.

--- wp-content/themes/dorsWP/header.php
+++ wp-content/themes/dorsWP/header.php
@@ -5,8 +5,15 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<!-- <link rel="stylesheet" href="css/style.css"> -->
-	<title>Главная</title>
+	<title>
+    <?php
+      if(is_404()){
+        echo "Ошибка 404";
+      } else {
+        the_title();
+      }
+    ?>
+  </title>
   <?php wp_head(); ?>
 </head>
 
